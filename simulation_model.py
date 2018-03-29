fed2f681dc0b3f508e3a4cc3ef0879284eec97da
bowen <zazakbw@gmail.com>
2018-03-29 20:22:49 +0200

    added randomize camera(per image) angle and randomize objects(per set of actions)

--- simulation_model.py
+++ simulation_model.py
@@ -4,8 +4,9 @@ parentdir = os.path.dirname(os.path.dirname(currentdir))
 os.sys.path.insert(0,parentdir)
 
 import pybullet as p
+import pybullet_data
 import numpy as np
-from numpy.random import normal
+from numpy.random import normal,uniform
 import random
 import copy
 import math
@@ -15,6 +16,10 @@ import cv2
 from pprint import pprint
 from pybullet_envs.bullet.kukaGymEnv import KukaGymEnv
 from pybullet_envs.bullet.kukaCamGymEnv import KukaCamGymEnv
+#gym envrionment information: 
+#   plane base [0,0,-1]
+#   tray base [0.640000,0.075000,-0.190000]
+#
 
 def add_one_obj_to_scene(num,pos=None, orientation = None, global_scaling=1):
     """
@@ -51,7 +56,8 @@ def add_objs_to_scene(nums,poses,orientations=None):
         
 def add_random_objs_to_scene(size,pos_mean=[0,0],pos_height=1,orientation_mean=[0,0,0,1]):
     nums = [random.randint(0,999) for _ in range(size)]
-    poses = [[normal(0.6,0.1),normal(scale=0.16),normal(0.05,0.05)] for _ in range(size)]#container center position (0.6,0,0)
+    poses = [[normal(0.64,0.14),normal(scale=0.14),-0.182] for _ in range(size)]#container center position (0.6,0,0)
+    # poses = [[uniform]]
     orientations = [normal(size=(4)) for _ in range(size)]
     objs = add_objs_to_scene(nums,poses,orientations)
     # objs = add_objs_to_scene(nums,poses)
@@ -67,6 +73,15 @@ def write_from_npimg(npimg,serial_number,path="sim_images/{0:0>6}.jpeg"):
     img = np.reshape(npimg, (512, 640, 4)).astype(np.uint8)#BGRA
     img = cv2.cvtColor(img,cv2.COLOR_BGRA2RGB)#RGB
     cv2.imwrite(path.format(serial_number),img)
+
+def get_randomized_ViewMat(sigma=0.01):
+    mean = [0,0,0]
+    cov = sigma*np.eye(3)
+    camEyePos = np.array([0.03,0.236,0.54]) + np.random.multivariate_normal(mean,cov)
+    targetPos = np.array([0.640000,0.075000,-0.190000]) + np.random.multivariate_normal(mean,cov)
+    cameraUp = np.array([0,0,1])
+    viewMat = p.computeViewMatrix(camEyePos,targetPos,cameraUp)
+    return viewMat
 
 def main_usingEnvOnly():
     environment = KukaGymEnv(renders=True,isDiscrete=False, maxSteps = 10000000)
@@ -87,22 +102,16 @@ def main_usingEnvOnly():
 
     done = False
     #According to the hand-eye coordination paper, the camera is mounted over the shoulder of the arm
-    # camEyePos = [0.03,0.236,0.54]
-    # distance = 1.06
-    # pitch=-56
-    # yaw = 258
-    # roll=0
-    # upAxisIndex = 2
-    # camInfo = p.getDebugVisualizerCamera()
-    # viewMat = camInfo[2]
-    # viewMat = p.computeViewMatrixFromYawPitchRoll(camEyePos,distance,yaw, pitch,roll,upAxisIndex)
-    # projMatrix = camInfo[3]
-    # print(viewMat)
-    # print(projMatrix)
-    viewMat = [-0.5120397806167603, 0.7171027660369873, -0.47284144163131714, 0.0, -0.8589617609977722, -0.42747554183006287, 0.28186774253845215, 0.0, 0.0, 0.5504802465438843, 0.8348482847213745, 0.0, 0.1925382763147354, -0.24935829639434814, -0.4401884973049164, 1.0]
-    projMatrix = [0.75, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 0.0, 0.0, -0.02000020071864128, 0.0]
+    camEyePos = [0.03,0.236,0.54]
+    targetPos = [0.640000,0.075000,-0.190000]
+    cameraUp = [0,0,1]
+    viewMat = p.computeViewMatrix(camEyePos,targetPos,cameraUp)
+    camInfo = p.getDebugVisualizerCamera()
+    projMatrix = camInfo[3]
+    # viewMat = [-0.5120397806167603, 0.7171027660369873, -0.47284144163131714, 0.0, -0.8589617609977722, -0.42747554183006287, 0.28186774253845215, 0.0, 0.0, 0.5504802465438843, 0.8348482847213745, 0.0, 0.1925382763147354, -0.24935829639434814, -0.4401884973049164, 1.0]
+    # projMatrix = [0.75, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 0.0, 0.0, -0.02000020071864128, 0.0]
     img_arr = p.getCameraImage(640,512,viewMatrix=viewMat,projectionMatrix=projMatrix)#640*512*3 
-    write_from_imgarr(img_arr, 1)
+    # write_from_imgarr(img_arr, 1)
     
     while (not done):   
         action=[]
@@ -111,11 +120,11 @@ def main_usingEnvOnly():
         state, reward, done, info = environment.step2(action)
         obs = environment.getExtendedObservation()
 
-
     # physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
     # p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
     # p.setGravity(0,0,-10)
     # planeId = p.loadURDF("plane.urdf")
+    # objects = p.loadSDF(os.path.join(pybullet_data.getDataPath(),"kuka_iiwa/kuka_with_gripper2.sdf"))
     
     # cubeStartPos = [0,0,1]
     # cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
@@ -130,10 +139,17 @@ def main_usingEnvOnly():
     # print(cubePos,cubeOrn)
     # p.disconnect()
 
+
 def main():
     environment = KukaCamGymEnv(renders=True,isDiscrete=False)  
     randomObjs = add_random_objs_to_scene(10)
     motorsIds = []
+    #addUserDebugParameter(paramName,rangeMin,rangeMax,startValue)
+    #return the most up-to-date reading of the parameter
+
+    #TODO: add visual randomization(texture,color,lighting,brightness) and dynamics randomization(mass, friction)
+    #usefule methods: setDebugObjectColor, changeDynamics
+
     #motorsIds.append(environment._p.addUserDebugParameter("posX",0.4,0.75,0.537))
 	#motorsIds.append(environment._p.addUserDebugParameter("posY",-.22,.3,0.0))
 	#motorsIds.append(environment._p.addUserDebugParameter("posZ",0.1,1,0.2))
@@ -146,7 +162,12 @@ def main():
     motorsIds.append(environment._p.addUserDebugParameter("yaw",-dv,dv,0))
     motorsIds.append(environment._p.addUserDebugParameter("fingerAngle",0,0.3,.3))	
     done = False
-    img_serial_num = 0
+    try:
+        with open("sim_images/serial_num_log.txt",'r') as f:
+            img_serial_num = int(f.read())
+    except:
+        print("read serial number failed!")
+        img_serial_num = 0
     step = 0
     snapshot_interval = 20
     while (not done):    
@@ -155,16 +176,27 @@ def main():
             action.append(environment._p.readUserDebugParameter(motorId))
         if step%snapshot_interval==0:
             #get cameta image
-            viewMat = [-0.5120397806167603, 0.7171027660369873, -0.47284144163131714, 0.0, -0.8589617609977722, -0.42747554183006287, 0.28186774253845215, 0.0, 0.0, 0.5504802465438843, 0.8348482847213745, 0.0, 0.1925382763147354, -0.24935829639434814, -0.4401884973049164, 1.0]
-            projMatrix = [0.75, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 0.0, 0.0, -0.02000020071864128, 0.0]
+            print("Saving image...")
+            viewMat = get_randomized_ViewMat(std = 0.01)
+            camInfo = p.getDebugVisualizerCamera()
+            projMatrix = camInfo[3]
             img_arr = p.getCameraImage(640,512,viewMatrix=viewMat,projectionMatrix=projMatrix)#640*512*3 
             write_from_imgarr(img_arr, img_serial_num)
             # np_img = environment.getExtendedObservation()#shape is not correct
             # write_from_npimg(np_img,img_serial_num)
             img_serial_num +=1
+            # environment._kuka.
         step +=1
         state, reward, done, info = environment.step(action)
         obs = environment.getExtendedObservation()
+        # print("done: {}, step: {}".format(done,step))
+        if done:
+            environment._reset()
+            randomObjs = add_random_objs_to_scene(10)
+            done =False
+            with open("sim_images/serial_num_log.txt","w") as f:
+                f.write(str(img_serial_num))
 if __name__ == '__main__':
-    main()
+    # main()
+    main_usingEnvOnly()
     
